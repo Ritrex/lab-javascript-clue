5a4c752323ce410ba615253a98385ac711b08c96
Ritrex <roalretre@gmail.com>
2019-07-25 18:20:20 -0500

    conturctores

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,3 +1,4 @@
+let rope = {};
 // Characters
 /*
 mrGreen
@@ -56,17 +57,19 @@ occupation:   Retired Football player
 
 // Weapons
 
-name: rope        ---   weight: 10
-name: knife       ---   weight: 8
-name: candlestick ---   weight: 2
-name: dumbbell    ---   weight: 30
-name: poison      ---   weight: 2
-name: axe         ---   weight: 15
-name: bat         ---   weight: 13
-name: trophy      ---   weight: 25
-name: pistol      ---   weight: 20
+name: "rope"        ---   weight: 10
+name: "knife"       ---   weight: 8
+name: "candlestick" ---   weight: 2
+name: "dumbbell"    ---   weight: 30
+name: "poison"      ---   weight: 2
+name: "axe"         ---   weight: 15
+name: "bat"         ---   weight: 13
+name: "trophy"      ---   weight: 25
+name: "pistol"      ---   weight: 20
+
 
 // Rooms
+let
 name: Dinning Room
 name: Conservatory
 name: Kitchen
@@ -84,11 +87,211 @@ name: Guest House
 name: Patio
 */
 
-// Characters Collection
-var charactersArray = [];
+class character {
+  constructor(fn, ln, col, desc, ag, img, occ) {
+    this.first_name = fn;
+    this.last_name = ln;
+    this.color = col;
+    this.description = desc;
+    this.age = ag;
+    this.image = img;
+    this.occupation = occ;
+  }
+}
 
+class weapon {
+  constructor(n, w) {
+    this.name = n;
+    this.weight = w;
+  }
+}
+class place {
+  constructor(nam) {
+    this.name = nam;
+  }
+}
+
+function personajeVacio() {
+  return new character("", "", "", "", 0, "", "");
+}
+
+function pregencharacter(perso) {
+  switch (perso) {
+    case "mrGreen":
+      return new character(
+        "Jacob",
+        "Green",
+        "green",
+        "He has a lot of connections",
+        45,
+        "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+        "Entrepreneur"
+      );
+
+      break;
+    case "drOrchid":
+      return new character(
+        "Doctor",
+        "Orchid",
+        "white",
+        "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+        26,
+        "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+        "Scientist"
+      );
+      break;
+    case "mrsPeacock":
+      return new character(
+        "Eleanor",
+        "Peacock",
+        "blue",
+        "She is from a wealthy family and uses her status and money to earn popularity",
+        36,
+        "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+        "Socialité"
+      );
+      break;
+    case "missScarlet":
+      return new character(
+        "kasandra",
+        "Scarlet",
+        "red",
+        "She is an A-list movie star with a dark past",
+        31,
+        "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+        "Actor"
+      );
+      break;
+    case "mrMustard":
+      return new character(
+        "Jack",
+        "Mustard",
+        "yellow",
+        "He is a former football player who tries to get by on his former glory",
+        62,
+        "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+        "Retired Football player"
+      );
+      break;
+    case "profPlum":
+      return new character(
+        "Victor",
+        "Plum",
+        "purple",
+        "Billionare video game designer",
+        22,
+        "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+        "Designer"
+      );
+      break;
+    default:
+    case "":
+      return new character("", "", "", "", 0, "", "");
+      break;
+  }
+}
+
+function pw(wea) {
+  switch (wea) {
+    case "rope":
+      return new weapon("rope", 10);
+
+      break;
+    case "knife":
+      return new weapon("knife", 8);
+      break;
+    case "candlestick":
+      return new weapon("candlestick", 2);
+      break;
+    case "dumbbell":
+      return new weapon("dumbbell", 30);
+      break;
+    case "poison":
+      return new weapon("poison", 2);
+      break;
+    case "axe":
+      return new weapon("axe", 15);
+      break;
+    case "bat":
+      return new weapon("bat", 13);
+      break;
+    case "trophy":
+      return new weapon("trophy", 25);
+      break;
+    case "pistol":
+      return new weapon("pistol", 20);
+      break;
+    default:
+    case "":
+      return new weapon("", 0);
+      break;
+  }
+}
+
+//let green=new character()
+
+var charactersArray = [
+  pregencharacter("mrGreen"),
+  pregencharacter("drOrchid"),
+  pregencharacter("profPlum"),
+  pregencharacter("missScarlet"),
+  pregencharacter("mrsPeacock"),
+  pregencharacter("mrMustard")
+];
+var weaponsArray = [
+  pw("rope"),
+  pw("knife"),
+  pw("candlestick"),
+  pw("dumbbell"),
+  pw("poison"),
+  pw("axe"),
+  pw("bat"),
+  pw("trophy"),
+  pw("pistol")
+];
 // Rooms' Collection
-var roomsArray = [];
+var roomsArray = [
+  new place("Dinning Room"),
+  new place("Conservatory"),
+  new place("Kitchen"),
+  new place("Study"),
+  new place("Library"),
+  new place("Billiard Room"),
+  new place("Hall"),
+  new place("Spa"),
+  new place("Living Room"),
+  new place("Observatory"),
+  new place("Theater"),
+  new place("Guest House"),
+  new place("Patio")
+];
+/*];
 
 // Weapons Collection
-var weaponsArray = [];
+*/
+
+function randomSelector(arr) {
+  //console.log(Math.floor(Math.random()*10))
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function pickMistery() {
+  return [
+    randomSelector(charactersArray),
+    randomSelector(weaponsArray),
+    randomSelector(roomsArray)
+  ];
+}
+function revealMistery(me) {
+  let homicida = me[0];
+  let arma = me[1];
+  let lugar = me[2];
+  return `${homicida.first_name} ${
+    homicida.last_name
+  } killed Mr.Boddy using the ${arma.name} in the ${lugar.name}!!!!`;
+}
+
+var misteryEnvelope = pickMistery();
+console.log(randomSelector(weaponsArray).name.name);
+console.log(pickMistery());
+let char = personajeVacio();
